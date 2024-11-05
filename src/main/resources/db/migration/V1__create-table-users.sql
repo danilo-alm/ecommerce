cdc68bac4adc5ae758982ba4c39e5457c40938e0
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-11-05 14:23:44 -0300

    refactor(db): enforce more constraints

--- src/main/resources/db/migration/V1__create-table-users.sql
+++ src/main/resources/db/migration/V1__create-table-users.sql
@@ -1,19 +1,19 @@
 CREATE TABLE users(
     id SERIAL,
-    username VARCHAR(36) NOT NULL PRIMARY KEY,
+    username VARCHAR(36) NOT NULL UNIQUE,
     password BINARY(60) NOT NULL,
-    enabled BOOLEAN NOT NULL,
+    enabled BOOLEAN NOT NULL DEFAULT TRUE,
     email VARCHAR(254) NOT NULL UNIQUE,
     full_name VARCHAR(100) NOT NULL,
-    phone_number VARCHAR(15),
+    phone_number VARCHAR(15) NOT NULL,
 
-    currency VARCHAR(3) DEFAULT "USD",
-    language VARCHAR(13) DEFAULT "en",
+    currency VARCHAR(3) NOT NULL DEFAULT "USD",
+    language VARCHAR(13) NOT NULL DEFAULT "en",
 
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP,
 
-    failed_login_attempts INT DEFAULT 0
+    failed_login_attempts INT NOT NULL DEFAULT 0
 );
 
 CREATE INDEX idx_users_email ON users(email);
