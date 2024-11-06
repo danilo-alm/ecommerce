84db3e64747fa7290e7defd49fb465bbb40d0d37
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-11-06 16:24:22 -0300

    fix: user loading should be transactional
    lazy loading results in error at login

--- src/main/java/com/danilo/ecommerce/config/CustomUserDetailsService.java
+++ src/main/java/com/danilo/ecommerce/config/CustomUserDetailsService.java
@@ -3,6 +3,7 @@ package com.danilo.ecommerce.config;
 
 import com.danilo.ecommerce.domain.user.User;
 import com.danilo.ecommerce.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -15,6 +16,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(() -> new
             UsernameNotFoundException(username));
