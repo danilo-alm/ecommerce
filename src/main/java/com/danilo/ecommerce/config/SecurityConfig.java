59748eec0bf4b7d01487a0cb78d02f9002ed9697
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-11-06 10:49:48 -0300

    feat: basic security config

---
+++ src/main/java/com/danilo/ecommerce/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.danilo.ecommerce.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(requests -> requests
+            .requestMatchers(HttpMethod.POST, "/user").permitAll()
+            .anyRequest().authenticated()
+        );
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
