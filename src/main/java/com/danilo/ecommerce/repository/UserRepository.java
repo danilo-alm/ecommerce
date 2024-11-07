d7f851da43f6fabcfccadd9fa6f12d357e8c9b7c
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-11-07 13:38:16 -0300

    fix: primary key type

--- src/main/java/com/danilo/ecommerce/repository/UserRepository.java
+++ src/main/java/com/danilo/ecommerce/repository/UserRepository.java
@@ -3,9 +3,9 @@ package com.danilo.ecommerce.repository;
 import com.danilo.ecommerce.domain.user.User;
 import org.springframework.data.repository.CrudRepository;
 
-import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Optional;
 
-public interface UserRepository extends CrudRepository<User, BigDecimal> {
+public interface UserRepository extends CrudRepository<User, BigInteger> {
     Optional<User> findByUsername(String username);
 }
