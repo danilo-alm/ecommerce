0ef0115abb2be759c4728c50157f2a7c17f21759
Danilo Almeida <cs.daniloalmeida@gmail.com>
2024-11-17 11:51:06 -0300

    feat: disable endpoint security for now

--- src/main/java/com/danilo/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/danilo/ecommerce/config/SecurityConfig.java
@@ -3,8 +3,8 @@ package com.danilo.ecommerce.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -17,9 +17,10 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests -> requests
-            .requestMatchers(HttpMethod.POST, "/user").permitAll()
+            .requestMatchers("/**").permitAll()
             .anyRequest().authenticated()
         );
+        http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
